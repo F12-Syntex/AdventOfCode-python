d39aaea8ed899b2329a5ffc8861b1123df6bafd9
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-11-30 23:39:14 +0000

    Setting up for AoC 2023

---
+++ advent_of_code/2015/day1/day1.py
@@ -0,0 +1,18 @@
+class Day1:
+    def __init__(self):
+        pass
+
+    def solve_part1(self):
+        pass
+
+    def solve_part2(self):
+        pass
+
+    def loadInputFiles():
+        COOKIE = ""
+        pass
+
+
+# Usage example:
+solver = Day1()
+solver.loadInputFiles()
