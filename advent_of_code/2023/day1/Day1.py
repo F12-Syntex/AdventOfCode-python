84451078a9b5e4889d084e9070ddc42351b7df0f
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-12-01 08:16:19 +0000

    Day1 2023
    horrible solution, but was done quick enough to get points, need to refractor

--- advent_of_code/2023/day1/Day1.py
+++ advent_of_code/2023/day1/Day1.py
@@ -5,11 +5,129 @@ class Day1:
         self.input_content = None
 
     def solve_part1(self):
-        print(self.input_content)
-        return
+
+        sum = 0
+
+        for line in self.input_content.splitlines():
+            #fins the first number in the string starting from the left
+
+            firstNum = None
+            secondNum = None
+
+            for i in range(len(line)):
+                if line[i].isdigit():
+                    first_num = line[i]
+                    break
+
+            
+            #finds the second number in the string starting from the right
+            for i in range(len(line)-1, -1, -1):
+                if line[i].isdigit():
+                    second_num = line[i]
+                    break
+            
+            joinedNum = first_num + "" + second_num
+
+            #converts the joined number to an integer
+            joinedNum = int(joinedNum)
+            sum += joinedNum
+
+
+        return sum
 
     def solve_part2(self):
-        pass
+
+        sum = 0
+
+        for line in self.input_content.splitlines():
+            #fins the first number in the string starting from the left
+
+            firstNum = None
+            secondNum = None
+
+            for i in range(len(line)):
+                #make a string from index 0 to this index
+                substring = line[0:i+1]
+                
+                if "one" in substring:
+                    first_num = str(1)
+                    break
+                if "two" in substring:
+                    first_num = str(2)
+                    break
+                if "three" in substring:
+                    first_num = str(3)
+                    break
+                if "four" in substring:
+                    first_num = str(4)
+                    break
+                if "five" in substring:
+                    first_num = str(5)
+                    break
+                if "six" in substring:
+                    first_num = str(6)
+                    break
+                if "seven" in substring:
+                    first_num = str(7)
+                    break
+                if "eight" in substring:
+                    first_num = str(8)
+                    break
+                if "nine" in substring:
+                    first_num = str(9)
+                    break
+                
+
+                if line[i].isdigit():
+                    first_num = line[i]
+                    break
+            
+            
+            #finds the second number in the string starting from the right
+            for i in range(len(line)-1, -1, -1):
+
+                substring = line[i:len(line)]
+
+                if "one" in substring:
+                    second_num = str(1)
+                    break
+                if "two" in substring:
+                    second_num = str(2)
+                    break
+                if "three" in substring:
+                    second_num = str(3)
+                    break
+                if "four" in substring:
+                    second_num = str(4)
+                    break
+                if "five" in substring:
+                    second_num = str(5)
+                    break
+                if "six" in substring:
+                    second_num = str(6)
+                    break
+                if "seven" in substring:
+                    second_num = str(7)
+                    break
+                if "eight" in substring:
+                    second_num = str(8)
+                    break
+                if "nine" in substring:
+                    second_num = str(9)
+                    break
+
+                if line[i].isdigit():
+                    second_num = line[i]
+                    break
+
+            joinedNum = first_num + "" + second_num
+
+            #converts the joined number to an integer
+            joinedNum = int(joinedNum)
+            sum += joinedNum
+
+
+        return sum
 
     def loadInputFiles(self):
         inputPath = os.path.join(os.getcwd(), "advent_of_code", "2023", "day1", "input.txt")
