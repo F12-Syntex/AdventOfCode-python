ff26d02bcae4a18f186fb8be333b1993200edf23
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-12-15 05:53:14 +0000

    Completed day 15 of advent of code
    part1: 00:06:16
    part2: 0:41:11

--- advent_of_code/2023/day15/Day15.py
+++ advent_of_code/2023/day15/Day15.py
@@ -1,20 +1,81 @@
 import os
 
+
+# Determine the ASCII code for the current character of the string.
+# Increase the current value by the ASCII code you just determined.
+# Set the current value to itself multiplied by 17.
+# Set the current value to the remainder of dividing itself by 256.
+
 class Day15:
     def __init__(self):
         self.input_content = None
 
     def solve_part1(self):
-        self.loadGrid()
-        self.prettyPrintGrid()
-        
-        return 0
+        res = 0
+        for word in self.input_content.split(","):
+            res += self.hash(word)
+            
+        return res
+
+    def hash(self, word):
+        cres = 0
+        for c in word:
+            cres += ord(c)
+            cres *= 17
+            cres %= 256
+        return cres
     
     def solve_part2(self):
-        return 0
+        res = 0
+        mapping = {}
+        for word in self.input_content.split(","):
+            if '=' in word:
+                label, value = word.split("=")
+                box = self.hash(label)
+                
+                data = (label, value)
+                
+                if box not in mapping:
+                    mapping[box] = [data]
+                else:
+                    arr = mapping[box]
+                    flag = False
+                    index = 0
+                    for item in arr:
+                        if item[0] == label:
+                            mapping[box][index] = data
+                            flag = True
+                        index += 1
+                    
+                    if not flag:
+                        arr.append(data)
+                        
+                    mapping[box] = arr
+            
+            if '-' in word:
+                label, value = word.split("-")
+                
+                box = self.hash(label)
+                if box in mapping:
+                    arr = mapping[box]
+                    for item in arr:
+                        if item[0] == label:
+                            arr.remove(item)
+                    mapping[box] = arr
+            # print(mapping)    
+        
+        for key in mapping:
+            arr = mapping[key]
+            for slot in range(len(arr)):
+                item = arr[slot]
+                focus = (1+int(key))*(1+int(slot))*int(item[1])
+                # print((1+key), " * ", (1+slot), " * ", item[1], " = ", focus)
+                res += focus
+                
+        return res
 
     def loadInputFiles(self):
-        inputPath = os.path.join(os.getcwd(), "2023", "day15", "test.txt")
+        inputPath = os.path.join(os.getcwd(), "2023", "day15", "input.txt")
         with open(inputPath, "r") as f:
             self.input_content = f.read()
             
