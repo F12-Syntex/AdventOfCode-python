61f485a4aab251f36309640cf0b0c63b2f0157de
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-12-15 07:56:32 +0000

    Compled day 12 of advent of code
    part1: 2:08:10+
    part2: N/A

--- advent_of_code/2023/day12/Day12.py
+++ advent_of_code/2023/day12/Day12.py
@@ -1,51 +1,67 @@
+import collections
 import os
 
 class Day12:
     def __init__(self):
         self.input_content = None
-    
-    def verify(self, dots, blocks):
-        curr = 0
-        visited = []
-        for character in dots:
-            if character == ".":
-                if curr > 0:
-                    visited.append(curr)
-                curr = 0
-            elif character == "#":
-                curr += 1
-            else:
-                assert False
-        
-        if curr > 0:
-            visited.append(curr)
-        
-        return visited == blocks
-    
-    def find(self, dots, blocks, index):
-        if index == len(dots):
-            return 1 if self.verify(dots, blocks) else 0
-        if dots[index] == "?":
-            return (self.find(dots[:index] + "#" + dots[index+1:], blocks, index + 1) + 
-                    self.find(dots[:index] + "." + dots[index+1:], blocks, index + 1))
-        else:
-            return self.find(dots, blocks, index + 1)
+        self.cache = {}
         
     def solve_part1(self):
         res = 0
+
         for line in self.input_content.splitlines():
-            springs = line.split(" ")[0]
-            groups = list(map(int, line.split(" ")[1].split(",")))
+            onsens, groups = line.split()
+            groups = tuple(map(int, groups.split(",")))
             
-            score = self.find(springs, groups, 0)
-            res += score
+            res += self.permutations(onsens, groups)
+
         return res
     
     def solve_part2(self):
-        return 0
+        res = 0
+
+        for line in self.input_content.splitlines():
+            onsens, groups = line.split()
+            groups = tuple(map(int, groups.split(",")))
+            
+            onsens = "?".join([onsens] * 5)
+            groups *= 5
+            
+            res += self.permutations(onsens, groups)
+    
+        return res
+    
+    def permutations(self, onsens, groups):
+        
+        res = 0
+        #base case if onsens are empty
+        if onsens == "":
+            return 1 if groups == () else 0
+        
+        #base case if groups are empty
+        if groups == ():
+            return 1 if "#" not in onsens else 0
+        
+        key = (onsens, groups)
+        
+        if key in self.cache:
+            return self.cache[key]
+        
+        #pretend a dot is a question mark
+        if onsens[0] in "?.":
+            res += self.permutations(onsens[1:], groups)
+        
+        #pretend a dot is a pound
+        if onsens[0] in "#?":
+            if groups[0] <= len(onsens) and "." not in onsens[:groups[0]] and (groups[0] == len(onsens) or onsens[groups[0]] != "#"):
+                res += self.permutations(onsens[groups[0] + 1:], groups[1:])
+            
+        self.cache[key] = res
+        
+        return res
 
     def loadInputFiles(self):
-        inputPath = os.path.join(os.getcwd(), "2023", "day12", "test.txt")
+        inputPath = os.path.join(os.getcwd(), "2023", "day12", "input.txt")
         with open(inputPath, "r") as f:
             self.input_content = f.read()
         
@@ -54,7 +70,7 @@ class Day12:
 solver = Day12()
 solver.loadInputFiles()
 part1_result = solver.solve_part1()
-part2_result = solver.solve_part2()
-
 print("Solution to Part 1:", part1_result)
+
+part2_result = solver.solve_part2()
 print("Solution to Part 2:", part2_result)
