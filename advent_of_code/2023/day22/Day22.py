827af066802e38d8eac2859090dc6ce473cf6b1b
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-12-22 11:43:23 +0000

    feat: complete day 22 of year 2023

--- advent_of_code/2023/day22/Day22.py
+++ advent_of_code/2023/day22/Day22.py
@@ -1,3 +1,4 @@
+from collections import deque
 import os
 import string
 import time
@@ -12,121 +13,101 @@ class Day22:
         self.YEAR = 2023
         self.aoc_utils = AOCInputGrabber(self.YEAR, self.DAY)
 
+        self.X_START = 0
+        self.Y_START = 1
+        self.Z_START = 2
+        self.X_END = 3
+        self.Y_END = 4
+        self.Z_END = 5
+
     def solve_part1(self):
 
-        grid = []
+
+        bricks = self.parseInput()
+        self.simulate_falling(bricks)
         
-        bricks = []
-        labels = []
+        #find all the brick to support and support to relationships
+        keys, values = self.findSupports(bricks)
 
-        index = 0
-        for line in self.input_content.splitlines():
-            start, end = line.split("~")
-            start = list(map(int, start.split(",")))
-            end = list(map(int, end.split(",")))
-            bricks.append((start, end))
-            labels.append(string.ascii_uppercase[index])
-            index += 1
-
-        max_x = max(max(start[0], end[0]) for start, end in bricks)
-        max_y = max(max(start[1], end[1]) for start, end in bricks)
-        max_z = max(max(start[2], end[2]) for start, end in bricks)
-
-        grid = [[[None for _ in range(max_z + 1)] for _ in range(max_x + 1)] for _ in range(max_y + 1)]
-
-        for (x1, y1, z1), (x2, y2, z2) in bricks:
-            for x in range(min(x1, x2), max(x1, x2) + 1):
-                for y in range(min(y1, y2), max(y1, y2) + 1):
-                    for z in range(min(z1, z2), max(z1, z2) + 1):
-                        grid[y][x][z] = (x1, y1, z1, x2, y2, z2)
-
-        supporting = {}
-
-        for supporting_brick_index in range(len(bricks)):
-            supporting_brick = bricks[supporting_brick_index]
-
-            supporting_bricks = []
-            for brick_index in range(len(bricks)):
-                brick = bricks[brick_index]
-
-                if not self.is_supporting(supporting_brick, brick):
-                    continue
-
-                # print(labels[supporting_brick_index], "supporting", labels[brick_index], "?")
-                supporting_bricks.append(brick_index)
-
-            supporting[supporting_brick_index] = supporting_bricks
-
-        supportedBy = {}
-        for k, v in supporting.items():
-
-            supported_by = []
-            for k2, v2 in supporting.items():
-                if k in v2:
-                    supported_by.append(k2)
-            supportedBy[k] = supported_by
-
-        
-        # print("Supporting:", supportedBy)
-        a = {}
-        for k, v in supportedBy.items():
-            # print(labels[k], "supported by", [labels[x] for x in v])
-            for value in v:
-                if value not in a:
-                    a[value] = []
-                a[value].append(set(list([labels[x] for x in v])))
-
-        snum = 0
-
-        for k, v in a.items():
-            # print(labels[k], "supported by", v)
-            for j in v:
-                if len(j) == 1:
-                    snum += 1
-
-        return len(bricks) - snum
-
-    #is brick1 supporting brick2?
-    def is_supporting(self, brick1, brick2):
-
-        brick1_start, brick1_end = brick1
-        brick2_start, brick2_end = brick2
-
-        b1x1, b1y1, b1z1 = brick1_start
-        b1x2, b1y2, b1z2 = brick1_end
-
-        b2x1, b2y1, b2z1 = brick2_start
-        b2x2, b2y2, b2z2 = brick2_end
-
-        b1zmax = max(b1z1, b1z2)
-        b2zmin = min(b2z1, b2z2) 
-
-        if brick1 == brick2:
-            return False
-        
-        for x in range(b1x1, b1x2+1):
-            for y in range(b1y1, b1y2+1):
-                if (x >= b2x1 and x <= b2x2) and (y >= b2y1 and y <= b2y2):
-                    if b1zmax < b2zmin:
-                        return True
-        
-        return False
-
+        #for every brick, count all bricks that support 2 or more bricks (which means removing them would not cause a chain reaction)
+        return sum(1 for i in range(len(bricks)) if all(len(values[j]) >= 2 for j in keys[i]))
 
     def solve_part2(self):
-        return 0
-    
-    def printGrid(self, grid):
-        for z in range(len(grid[0][0])):
-            print("\nz =", z)
-            for y in range(len(grid)):
-                line = ""
-                for x in range(len(grid[0])):
-                    if grid[y][x][z] is None:
-                        line += "."
-                    else:
-                        line += str(grid[y][x][z])
-                print(line)
+        bricks = self.parseInput()
+        self.simulate_falling(bricks)
+        
+        keys, values = self.findSupports(bricks)
+
+        res = 0
+
+        for i in range(len(bricks)):
+            queue= deque()
+
+            #Add all the bricks that will cause a chain reaction if disintegrated to a queue
+            for j in keys[i]:
+                if len(values[j]) == 1:
+                    queue.append(j)
+
+            #bfs we start with the brick we are trying to disintegrate
+            falling = set(queue)
+            falling.add(i)
+            
+            #for every brick in the queue, remove all the falling bricks that are supported by it
+            #then check if everything in falling is in the values for that brick
+            #add the length of the collision and add it to the queue
+            while queue:
+                j = queue.popleft()
+                for k in keys[j]:
+                    if k in falling:
+                        continue
+                    if values[k] <= falling:
+                        queue.append(k)
+                        falling.add(k)
+                    
+            
+            res += len(falling) - 1
+
+        return res
+
+    def simulate_falling(self, bricks):
+        for index, brick in enumerate(bricks):
+            max_z = 1
+            for check in bricks[:index]:
+                if self.intersects(brick, check):
+                    max_z = max(max_z, check[self.Z_END] + 1)
+            brick[self.Z_END] -= brick[self.Z_START] - max_z
+            brick[self.Z_START] = max_z
+            
+        bricks.sort(key=lambda brick: brick[2])
+
+    def findSupports(self, bricks):
+        keys = {i: set() for i in range(len(bricks))}
+        values = {i: set() for i in range(len(bricks))}
+
+        for j, top_brick in enumerate(bricks):
+            for i, bottom_brick in enumerate(bricks[:j]):
+                if self.intersects(bottom_brick, top_brick) and top_brick[self.Z_START] == bottom_brick[self.Z_END] + 1:
+                    keys[i].add(j)
+                    values[j].add(i)
+        return keys, values
+
+    def intersects(self, brick1, brick2):
+        return (max(brick1[self.X_START], brick2[self.X_START]) <= min(brick1[self.X_END], brick2[self.X_END])) and (max(brick1[self.Y_START], brick2[self.Y_START]) <= min(brick1[self.Y_END], brick2[self.Y_END]))
+
+    def parseInput(self):
+        bricks = []
+        for line in self.input_content.splitlines():
+            parts = line.split("~")
+            brick = []
+            for part in parts:
+                items = part.split(",")
+                for i in items:
+                    brick.append(int(i))
+
+            bricks.append(brick)
+
+        bricks.sort(key=lambda x: x[self.Z_START])
+        return bricks
 
     def loadInputFiles(self):
         inputPath = os.path.join(os.getcwd(), str(self.YEAR), "day"+str(self.DAY), "input.txt")
